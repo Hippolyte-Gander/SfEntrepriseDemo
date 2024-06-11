a756eea20557abc70c4509fe377f212b84b74a25
Hippolyte G <hippolyte.gander5@gmail.com>
2024-06-11 22:21:58 +0200

    ajout dans les forms

--- src/Controller/EmployeController.php
+++ src/Controller/EmployeController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use App\Entity\Employe;
 use App\Form\EmployeType;
 use App\Repository\EmployeRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -23,11 +25,24 @@ class EmployeController extends AbstractController
     }
 
     #[Route('/employe/new', name: 'new_employe')]
-    public function new(Request $request): Response
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $employe = new Employe();
 
         $form =$this->createForm(EmployeType::class, $employe);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $employe = $form->getData();
+            // prepare en PDO
+            $entityManager->persist($employe);
+            // execute en PDO
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_employe');
+        }
 
         return $this->render('employe/new.html.twig', [
             'formAddEmploye' => $form
