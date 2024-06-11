dbc732cdf579e5e6f9b51445abad7ad51452edc4
Hippolyte G <hippolyte.gander5@gmail.com>
2024-06-11 16:55:54 +0200

    form mais en place + fait plus présentable avec Bootstrap, mais pas encore instoré en BDD

--- src/Controller/EntrepriseController.php
+++ src/Controller/EntrepriseController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Entreprise;
+use App\Form\EntrepriseType;
 use App\Repository\EntrepriseRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -24,6 +26,18 @@ class EntrepriseController extends AbstractController
         ]);
     }
 
+    #[Route('/entreprise/new', name: 'new_entreprise')]
+    public function new(Request $request): Response
+    {
+        $entreprise = new Entreprise();
+
+        $form =$this->createForm(EntrepriseType::class, $entreprise);
+
+        return $this->render('entreprise/new.html.twig', [
+            'formAddEntreprise' => $form
+        ]);
+    }
+    
     #[Route('/entreprise/{id}', name: 'show_entreprise')]
     public function show(Entreprise $entreprise) : Response
     {
@@ -31,4 +45,5 @@ class EntrepriseController extends AbstractController
             'entreprise'=> $entreprise
         ]);
     }
+
 }
--- src/Form/EntrepriseType.php
+++ src/Form/EntrepriseType.php
@@ -2,36 +2,33 @@
 
 namespace App\Form;
 
-use App\Entity\Employe;
 use App\Entity\Entreprise;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
-class EmployeType extends AbstractType
+class EntrepriseType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom', TextType::class, [
+            ->add('raisonSociale', TextType::class, [
                 'attr' => [
                     'class' => 'form-control'
                 ]])
-            ->add('prenom', TextType::class, [
-                'attr' => [
-                    'class' => 'form-control'
-                ]])
-            ->add('dateNaissance', DateType::class, [
+            ->add('dateCreation', DateType::class, [
                 'widget' => 'single_text',
                 'attr' => [
                     'class' => 'form-control'
                 ]])
-            ->add('dateEmbauche', DateType::class, [
-                'widget' => 'single_text',
+            ->add('adresse', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ]])
+            ->add('cp', TextType::class, [
                 'attr' => [
                     'class' => 'form-control'
                 ]])
@@ -39,12 +36,6 @@ class EmployeType extends AbstractType
                 'attr' => [
                     'class' => 'form-control'
                 ]])
-            ->add('entreprise', EntityType::class, [
-                'class' => Entreprise::class,
-                'attr' => [
-                    'class' => 'form-control'
-                ]
-            ])
             ->add('Valider', SubmitType::class, [
                 'attr' => [
                     'class' => 'btn btn-success'
@@ -55,7 +46,7 @@ class EmployeType extends AbstractType
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => Employe::class,
+            'data_class' => Entreprise::class,
         ]);
     }
 }
